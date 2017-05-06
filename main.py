24b63da47c173a64620f78e880b58193e299905c
Victor Le <levictor3000@hotmail.fr>
2017-05-06 16:18:25 +0200

    Added conf file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 import sys
 import requests
+import yaml
 
 if __name__ == '__main__':
+
+    with open("config.yml", 'r') as yml_file:
+        cfg = yaml.load(yml_file)
+
     cmd = ""
     for i in range(1, len(sys.argv)):
         cmd += sys.argv[i] + " "
@@ -10,6 +15,6 @@ if __name__ == '__main__':
     cmd = cmd.strip()
     print(cmd)
 
-    req = requests.post('http://httpbin.org/post', data={'command': cmd})
+    req = requests.post(cfg['compilio_host'] + 'compiler/init', data={'command': cmd})
     print(req)
 
