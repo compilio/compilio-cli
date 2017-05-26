abc91ea2aeab4e0fc23c74c46edf583266364779
Quentin de Longraye <quentin@dldl.fr>
2017-05-26 12:25:29 +0200

    add version

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@
 
 from setuptools import setup
 
+VERSION = '0.1.0'
+
 setup(
     pbr=True,
+    version=VERSION,
     entry_points={
         'console_scripts': [
             'compilio=compilio.cli:main'
