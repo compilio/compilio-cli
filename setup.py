dc71de2f80cb5d460ba93b4404f3fac9d1a59373
Quentin de Longraye <quentin@dldl.fr>
2017-05-26 12:49:38 +0200

    stop using setup file.cfg file

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 VERSION = '0.1.0'
 
 setup(
-    pbr=True,
+    name='compilio-cli',
     version=VERSION,
-    entry_points={
-        'console_scripts': [
-            'compilio=compilio.cli:main'
-        ],
-    }
+    description='CLI tool to interact with Compilio API',
+    author='Quentin de Longraye',
+    url='https://github.com/compilio/compilio-cli',
+    license='GPL',
+    packages=find_packages(),
+    entry_points="""
+    [console_scripts]
+    compilio=compilio.cli:main
+    """,
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
 )
